359d7cb80ad81596383cc9d184cf1601b32ab257
kancharlaumadevi40-svg <kancharlaumadevi40@gmail.com>
2025-11-15 15:50:30 +0530

    modified Demo2 and added Demo3

--- src/Demo2.java
+++ src/Demo2.java
@@ -3,7 +3,7 @@ public class Demo2 {
 
 	public static void main(String[] args)
 	{
-		System.out.println("DEmo2");
+		System.out.println("DEmo2 ");
 		
 
 	}
---
+++ src/Demo5.java
@@ -0,0 +1,11 @@
+
+public class Demo5 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("rbg tecnnologies");
+		System.out.println("automachine testing");
+
+	}
+
+}
