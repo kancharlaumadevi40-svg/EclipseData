9cd84ad5ed0bcc208f11a5357ea4c39e87616afb
kancharlaumadevi40-svg <kancharlaumadevi40@gmail.com>
2025-11-15 15:53:32 +0530

    Update Demo1.java

--- src/Demo1.java
+++ src/Demo1.java
@@ -4,12 +4,17 @@ public class Demo1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("welcome ");
+		funB();
 		
 
 	}
 	public void funA()
 	{
 		System.out.println("funA");
+	}
+	public void funB()
+	{
+		System.out.println("funB");
 	}
 
 }
