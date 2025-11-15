d92fa6871363bbec6893623d18471b9495df6120
kancharlaumadevi40-svg <kancharlaumadevi40@gmail.com>
2025-11-15 15:54:10 +0530

    Create Demo6.java

---
+++ src/Demo6.java
@@ -0,0 +1,21 @@
+
+public class Demo1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("welcome ");
+		funB();
+    funA();
+		
+
+	}
+	public void funA()
+	{
+		System.out.println("funA");
+	}
+	public void funB()
+	{
+		System.out.println("funB");
+	}
+
+}
